068870310215230bc5773e58d96a85970835e96f
Harris042 <harrisabdullah2001@gmail.com>
2022-08-23 04:48:41 +0500

    updating settings:

--- clearanceform_live/settings.py
+++ clearanceform_live/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)^evpa+979s&+0v6l4fk3!o$k1r43^(7&$0vp7fzx=endb!k6^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clearanceform.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
